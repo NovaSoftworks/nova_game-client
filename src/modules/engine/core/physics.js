d0c1b8988c67116f2ab9329f50fe4187384b245a
marclamy <marc.lamy83@gmail.com>
2023-07-26 22:53:55 +0000

    update the fixed update rate to 16ms

--- src/modules/engine/core/physics.js
+++ src/modules/engine/core/physics.js
@@ -2,7 +2,7 @@ export class Physics {
     /**
      * Interval in seconds between each physics simulation frame.
      */
-    static fixedTimeStep = 10 // ms
+    static fixedTimeStep = 16 // ms
 
     /**
      * The number of the last physics simulation frame.
