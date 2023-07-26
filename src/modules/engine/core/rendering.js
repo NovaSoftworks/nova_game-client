f83678d8213b05d31d26d46f3c044462583ffcf8
marclamy <marc.lamy83@gmail.com>
2023-07-26 23:10:15 +0000

    remove semicolons from the rendering module

--- src/modules/engine/core/rendering.js
+++ src/modules/engine/core/rendering.js
@@ -1,14 +1,14 @@
 export class Rendering {
-    static canvas;
-    static ctx;
+    static canvas
+    static ctx
 
     static setCanvas(canvasElement) {
-        Rendering.canvas = canvasElement;
-        Rendering.ctx = canvasElement.getContext('2d');
+        Rendering.canvas = canvasElement
+        Rendering.ctx = canvasElement.getContext('2d')
     }
 
     static clearCanvas() {
-        Rendering.ctx.fillStyle = "#202027";
-        Rendering.ctx.fillRect(0, 0, Rendering.canvas.width, Rendering.canvas.height);
+        Rendering.ctx.fillStyle = "#202027"
+        Rendering.ctx.fillRect(0, 0, Rendering.canvas.width, Rendering.canvas.height)
     }
 }
