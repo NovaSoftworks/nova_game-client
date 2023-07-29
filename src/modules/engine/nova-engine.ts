aa33c5ff617696a51864bf27c1c1f54fc5e5087f
marclamy <marc.lamy83@gmail.com>
2023-07-29 17:00:00 +0000

    refactor the NovaEngine class file

--- src/modules/engine/nova-engine.ts
+++ src/modules/engine/nova-engine.ts
@@ -3,55 +3,57 @@ import { Rendering } from './core/rendering'
 import { Time } from './core/time'
 import { UI } from './core/ui'
 import { World } from './ecs/world'
-import { Entity } from './ecs/entity'
 
 export class NovaEngine {
     static UI = UI
     static world = World.current
 
     static start(options = { width: 1280, height: 720 }, callback) {
-        init(options.width, options.height)
+        NovaEngine.setupCanvas(options.width, options.height)
 
         if (typeof callback === 'function') {
-            callback();
+            callback()
         }
 
-        window.requestAnimationFrame(update);
+        window.requestAnimationFrame(NovaEngine.update)
+    }
+
+    private static setupCanvas(canvasWidth, canvasHeight) {
+        const canvas = document.createElement('canvas')
+        canvas.width = canvasWidth
+        canvas.height = canvasHeight
+
+        const gameCanvasContainer = document.getElementById("game-canvas-container")
+        if (gameCanvasContainer) {
+            gameCanvasContainer.appendChild(canvas)
+        } else {
+            throw new Error("Could not attach the game to #game-canvas-container.")
+        }
+
+        Rendering.setCanvas(canvas)
+    }
+
+    private static accumulatedTime = 0
+    private static update() {
+        Time.calculateDeltaTime()
+        let dt = Time.deltaTime
+
+        NovaEngine.accumulatedTime += dt
+
+        while (NovaEngine.accumulatedTime >= Physics.fixedTimeStep) { // comparing ms with ms
+            NovaEngine.world.updateFixedSystems(Physics.fixedTimeStep / 1000)
+            NovaEngine.accumulatedTime -= Physics.fixedTimeStep
+            Physics.stepNumber++
+        }
+
+        Rendering.clearCanvas()
+        NovaEngine.world.updateSystems(dt / 1000)
+
+        window.requestAnimationFrame(NovaEngine.update)
     }
 }
 
 
-let accumulatedTime = 0
 
-function init(canvasWidth, canvasHeight) {
-    const canvas = document.createElement('canvas')
-    canvas.width = canvasWidth
-    canvas.height = canvasHeight
 
-    const gameCanvasContainer = document.getElementById("game-canvas-container")
-    if (gameCanvasContainer) {
-        gameCanvasContainer.appendChild(canvas)
-    } else {
-        throw new Error("Could not attach the game to #game-canvas-container.");
-    }
 
-    Rendering.setCanvas(canvas)
-}
-
-function update() {
-    Time.calculateDeltaTime()
-    let dt = Time.deltaTime
-
-    accumulatedTime += dt
-
-    while (accumulatedTime >= Physics.fixedTimeStep) { // comparing ms with ms
-        NovaEngine.world.updateFixedSystems(Physics.fixedTimeStep / 1000)
-        accumulatedTime -= Physics.fixedTimeStep;
-        Physics.stepNumber++
-    }
-
-    Rendering.clearCanvas()
-    NovaEngine.world.updateSystems(dt / 1000)
-
-    window.requestAnimationFrame(update)
-}
