e62c51220822f7bc078d4aa8d6b2259af9961645
marclamy <marc.lamy83@gmail.com>
2024-07-05 09:00:43 +0000

    add player spawn comments for future use

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,18 @@ world.createSystem(PhysicsSystem)
 world.createSystem(CircleRendererSystem)
 world.createSystem(InputSystem)
 world.createSystem(MoveSystem)
-world.createSystem(ColliderRendererSystem)
+// world.createSystem(ColliderRendererSystem)
 world.createSystem(NameplateRendererSystem)
+
+// const player = world.createEntity()
+
+// world.addComponent(player, new Transform(new Vector2(472, 262)))
+// world.addComponent(player, new Circle(16, 'orange'))
+// world.addComponent(player, new Collider(new Rectangle(32, 32)))
+// world.addComponent(player, new Velocity())
+// world.addComponent(player, new Nameplate('orange'))
+
+// console.log('Spawned player')
 
 window.requestAnimationFrame(gameLoop)
 
